da98c7693e2d9f93ad67d6c303775c423d5e7892
brian <brian@vpac.org>
2009-08-06 06:11:10 +0000

    Comment out code to deal with legacy data.

--- lintory/upload/windows/__init__.py
+++ lintory/upload/windows/__init__.py
@@ -262,6 +262,12 @@ def get_storage(disk_drive, computer):
     if new_query.count() > 0:
         query = new_query
     else:
+#        query = query.filter(
+#                auto_manufacturer__isnull=True,
+#                auto_model__isnull=True,
+#                total_size__isnull=True,
+#                sector_size__isnull=True,
+#        )
         raise import_error("Could not find storage - manufacturer, model, TotalSectors, or BytesPerSector mismatch")
 
     # did we get a valid result?
