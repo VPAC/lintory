95dc773080c65ba607765709697d6af7cf3f0bbf
brian <brian@vpac.org>
2010-06-25 06:24:06 +0000

    Update for CSRF.

--- sample/settings-template.py
+++ sample/settings-template.py
@@ -101,6 +101,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
 )
 
 ROOT_URLCONF = 'sample.urls'
