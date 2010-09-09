eae5845d875d7be54a58facc659d7225100e6c1c
brian <brian@vpac.org>
2010-09-09 01:55:55 +0000

    Update upload URL.

--- scripts/windows/get_and_upload.cmd
+++ scripts/windows/get_and_upload.cmd
@@ -4,7 +4,7 @@ net use %LUTILS%
 if errorlevel 1 exit /b 1
 cscript %LUTILS%\lintory\get_lintory.vbs > %TEMP%\lintory.txt
 if errorlevel 1 exit /b 1
-%LUTILS%\lintory\curl -T %TEMP%\lintory.txt -H "Content-Type: text/plain" --cacert \\hq\utils\certificates\apac\cacert.pem -o %TEMP%\lintory.html https://hq.in.vpac.org/inventory/upload/windows/
+%LUTILS%\lintory\curl -T %TEMP%\lintory.txt -H "Content-Type: text/plain" --cacert \\hq\utils\certificates\apac\cacert.pem -o %TEMP%\lintory.html https://hq.in.vpac.org/lintory/upload/windows/
 if errorlevel 1 exit /b 1
 
 rem net use %LUTILS% /d
